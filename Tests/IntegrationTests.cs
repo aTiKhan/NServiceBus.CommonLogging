3e745b95e942238d93649cedbf03304ef941fd43
Simon Cropp <simon.cropp@gmail.com>
2014-06-27 11:44:49 +0100

    dont need to run installers

--- Tests/IntegrationTests.cs
+++ Tests/IntegrationTests.cs
@@ -18,9 +18,7 @@ namespace Tests
 
             var configure = Configure.With(b => b.EndpointName("NServiceBusCommonLoggingTests"));
             configure.UseSerialization<Json>();
-            configure.UseTransport<Msmq>();
             configure.UsePersistence<InMemory>();
-            configure.EnableInstallers();
             using (var bus = configure.CreateBus())
             {
                 bus.Start();
