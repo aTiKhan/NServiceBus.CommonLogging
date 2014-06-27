adc70767cb6374feab9fdb43049abf7bc0a3d90d
Simon Cropp <simon.cropp@gmail.com>
2014-06-27 14:35:42 +0100

    tests

--- Tests/IntegrationTests.cs
+++ Tests/IntegrationTests.cs
@@ -4,26 +4,25 @@ using NServiceBus.CommonLogging;
 using NServiceBus.Persistence;
 using NUnit.Framework;
 
-namespace Tests
+
+[TestFixture]
+public class IntegrationTests
 {
-    [TestFixture]
-    public class IntegrationTests
+    [Test]
+    public void Ensure_log_messages_are_redirected()
     {
-        [Test]
-        public void Ensure_log_messages_are_redirected()
+        LogManager.Adapter = new MemoryAdapter();
+
+        CommonLoggingConfigurator.Configure();
+
+        var configure = Configure.With(b => b.EndpointName("NServiceBusCommonLoggingTests"));
+        configure.UseSerialization<Json>();
+        configure.UsePersistence<InMemory>();
+        using (var bus = configure.CreateBus())
         {
-            LogManager.Adapter = new MemoryAdapter();
-
-            CommonLoggingConfigurator.Configure();
-
-            var configure = Configure.With(b => b.EndpointName("NServiceBusCommonLoggingTests"));
-            configure.UseSerialization<Json>();
-            configure.UsePersistence<InMemory>();
-            using (var bus = configure.CreateBus())
-            {
-                bus.Start();
-                Assert.IsNotEmpty(MemoryLog.Messages);
-            }
+            bus.Start();
+            Assert.IsNotEmpty(MemoryLog.Messages);
         }
     }
 }
+
