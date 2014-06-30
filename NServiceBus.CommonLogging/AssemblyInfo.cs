ee6966346811ed1848f092700a7873b1674529be
Simon Cropp <simon.cropp@gmail.com>
2014-06-30 20:07:28 +0100

    fix assembly info

--- NServiceBus.CommonLogging/AssemblyInfo.cs
+++ NServiceBus.CommonLogging/AssemblyInfo.cs
@@ -1,4 +1,9 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 
-[assembly: AssemblyTitle("NServiceBus.CommonLogging")]
-[assembly: AssemblyProduct("NServiceBus.CommonLogging")]
+[assembly: AssemblyTitle("NServiceBus CommonLogging integration")]
+[assembly: AssemblyDescription("Implementation of logging functionality on top of CommonLogging.")]
+[assembly: AssemblyCopyright("Copyright 2010-2014 NServiceBus. All rights reserved")]
+[assembly: AssemblyProduct("NServiceBus")]
+[assembly: AssemblyCompany("NServiceBus Ltd.")]
+[assembly: CLSCompliant(true)]
