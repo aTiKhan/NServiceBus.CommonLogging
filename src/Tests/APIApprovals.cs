6e66827d9ef7c82a2ab48355afcf3fbb9033b831
Sean Feldman <feldman.sean@gmail.com>
2020-12-09 14:43:33 -0700

    Fix API Approvals

--- src/Tests/APIApprovals.cs
+++ src/Tests/APIApprovals.cs
@@ -9,7 +9,10 @@ public class APIApprovals
     [Test]
     public void Approve()
     {
-        var publicApi = ApiGenerator.GeneratePublicApi(Assembly.Load("NServiceBus.CommonLogging"), excludeAttributes: new[] { "System.Runtime.Versioning.TargetFrameworkAttribute" });
+        var publicApi = Assembly.Load("NServiceBus.CommonLogging").GeneratePublicApi(new ApiGeneratorOptions
+        {
+            ExcludeAttributes = new[] { "System.Runtime.Versioning.TargetFrameworkAttribute", "System.Reflection.AssemblyMetadataAttribute" }
+        });
         Approver.Verify(publicApi);
     }
 }
